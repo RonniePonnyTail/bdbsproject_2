d71fb7bebf130b6ac5d5f5bee706f0a3e8a7556e
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-12 21:01:57 +0100

    test

--- playlist.php
+++ playlist.php
@@ -1,7 +1,13 @@
 <?php
 include_once 'header.php';
 ?>
-
+<?php
+if(!isset($_SESSION['u_ID']))
+{
+    // not logged in
+    header('Location: idnex.php');
+    exit();
+}?>
 <section class="main-container">
 	<div class="main-wrapper">
 		<H2>Playlist</H2>
