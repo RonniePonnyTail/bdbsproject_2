eb95669911a552d642cd032e2b79aec8cab8a29c
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-14 10:18:08 +0100

    databaze hudby

--- databaze.php
+++ databaze.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'header.php';
+include 'includes/dbh.inc.php';
 ?>
 <?php
 if(!isset($_SESSION['u_ID']))
@@ -11,6 +12,29 @@ if(!isset($_SESSION['u_ID']))
 <section class="main-container">
 	<div class="main-wrapper">
 		<H2>Databaze</H2>
+		<?php
+		$result = mysqli_query($conn,"SELECT * FROM hudba");
+
+		echo '<table border="1" id="customers">
+		<tr>
+		<th>Kapela</th>
+		<th>Album</th>
+		<th>Pisen</th>
+		<th>Delka</th>
+		</tr>';
+
+		while($row = mysqli_fetch_array($result))
+		{
+		echo "<tr>";
+		echo "<td>" . $row['KAPELA'] . "</td>";
+		echo "<td>" . $row['ALBUM'] . "</td>";
+		echo "<td>" . $row['PISEN'] . "</td>";
+		echo "<td>" . $row['DELKA'] . "</td>";
+		echo "</tr>";
+		}
+		echo '</table>';
+
+		?>
 	</div>
 </section>
 
