6d7c1671c5f951c2e8d1d0e7a7243a243f86bd9f
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-14 09:29:17 +0100

    prihlasen

--- footer.php
+++ footer.php
@@ -1,2 +1,8 @@
 </body>
+<footer ><p class="footer-style">Copyright (C) 2018 Roman Halata</p>
+		 <?php if (isset($_SESSION['u_ID'])) {
+						echo "<a >Prihlasen: ".$_SESSION['u_EMAIL']."</a>";
+				}else{
+					 echo "<a >Neprihlasen</a>";} ?>
+</footer>
 </html>
